50a6972a672e0585d234dccad193b91920cb140e
John_V_Coder <omondijohn669@gmail.com>
2025-03-27 00:30:48 +0300

    initial commit

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+rooms = [
+    {'id':1, 'name':'lets learn python'},
+    {'id':1, 'name':'lets learn python'},
+    {'id':1, 'name':'lets learn python'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+    
+
+def room(request):
+    return render(request, 'base/room.html')
