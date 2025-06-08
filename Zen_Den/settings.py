6ffb08a5f867a701a60c064347e91b66603f9492
John_V_Coder <omondijohn669@gmail.com>
2025-06-08 09:16:50 +0300

    database clear

--- Zen_Den/settings.py
+++ Zen_Den/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-DEBUG = True 
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+if not ALLOWED_HOSTS[0]:  
+    ALLOWED_HOSTS = ['zen-den.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
