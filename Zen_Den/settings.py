89cb20ba64d77919a586108e4e61ea63ba38e44d
John_V_Coder <omondijohn669@gmail.com>
2025-06-08 11:28:31 +0300

    all changes

--- Zen_Den/settings.py
+++ Zen_Den/settings.py
@@ -143,18 +143,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join('staticfiles')
+
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join('static'),
+    os.path.join(BASE_DIR / 'static'),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 #STATIC_ROOT = 
 
